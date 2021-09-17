6e605541ea27f66b5ddbb98d191f5654c3868647
Ghosh <mauparna.ghosh@capgemini.com>
2021-09-17 10:01:43 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import labelData from '../assets/labelDetails.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  userForm:FormGroup;
-  upadteFname:String;
-  ngOnInit(){
-    this.userForm=new FormGroup({
-      fname:new FormControl('',[Validators.required,Validators.minLength(8),Validators.maxLength(30)]),
-      lname:new FormControl('',[Validators.required,Validators.minLength(8),Validators.maxLength(255)]),
-      des:new FormControl('',[Validators.minLength(8),Validators.maxLength(4000)])
-    })
+export class AppComponent implements OnInit {
+
+  userForm: FormGroup;
+  upadteFname: string;
+
+  // Using Data from the JSON File
+  labData = labelData;
+  // Adding form group and form control for react form
+  ngOnInit() {
+    this.userForm = new FormGroup({
+      fname: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(30)]),
+      lname: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(255)]),
+      des: new FormControl('', [Validators.minLength(8), Validators.maxLength(4000)])
+    });
   }
-  onChange(data){
-    if(data.target.value){
-      this.upadteFname=this.userForm.get('fname').value;
+
+  // Fetching the value of the name from the input field
+  onChange(data) {
+    if (data.target.value) {
+      this.upadteFname = this.userForm.get('fname').value;
     }
-    
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MaterialModule} from '../app/material/material.module'
+import {MaterialModule} from '../app/material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
